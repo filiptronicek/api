6719dbc3a233c805eb8ea64cbe65be2d1eb9cb50
Filip Troníček <filip.tronicek@seznam.cz>
2020-09-21 21:21:12 +0200

    Add bandwidth

--- api/bandwith.js
+++ api/bandwith.js
@@ -1,15 +1,26 @@
 const fetch = require("node-fetch");
 require("dotenv").config();
 
-const Auth = process.env.CF_TOKEN;
+const CFAuth = process.env.CF_TOKEN;
+const VCAuth = process.env.VERCEL_TOKEN;
 
 /* Config */
 const listEndpoint = "https://api.cloudflare.com/client/v4/zones";
-const token = "Bearer " + Auth;
-const fetchConfig = {
+const vercelEndpoint = "https://vercel.com/api/pricing/state/usage";
+
+const vercelToken = `Bearer ${VCAuth}`;
+const VercelFetchConfig = {
   method: "GET",
   headers: {
-    Authorization: token,
+    Authorization: vercelToken,
+  },
+};
+
+const cloudflareToken = `Bearer ${CFAuth}`;
+const CloudFlareFetchConfig = {
+  method: "GET",
+  headers: {
+    Authorization: cloudflareToken,
   },
 };
 
@@ -25,30 +36,37 @@ function formatBytes(bytes, decimals = 0) {
 module.exports = (_req, res) => {
   global.bandwidths = [];
 
-  /* Get all the zones */
+  fetch(vercelEndpoint, VercelFetchConfig)
+    .then((responce) => responce.json())
+    .then((Vresp) => {
+      const VCband = Vresp.metrics.bandwidth.rx + Vresp.metrics.bandwidth.tx;
 
-  fetch(listEndpoint, fetchConfig)
-    .then((response) => response.json())
-    .then((resp) => {
-      let i = 0;
-      resp.result.forEach((obj) => {
-        fetch(
-          `https://api.cloudflare.com/client/v4/zones/${obj.id}/analytics/dashboard?since=-44640`,
-          fetchConfig
-        )
-          .then((response) => response.json())
-          .then((resp) => bandwidths.push(resp.result.totals.bandwidth.all))
-          .then(() => {
-            const sum = bandwidths.reduce((a, b) => a + b, 0);
-            if (i++ === resp.result.length - 1) {
-              res.setHeader('Cache-Control', 's-maxage=86400');
-              res.status(200).send({
-                status: "success",
-                result: { bytes: sum, humanReadable: formatBytes(sum, 2) },
+      /* Get all the zones */
+
+      fetch(listEndpoint, CloudFlareFetchConfig)
+        .then((response) => response.json())
+        .then((resp) => {
+          let i = 0;
+          resp.result.forEach((obj) => {
+            fetch(
+              `https://api.cloudflare.com/client/v4/zones/${obj.id}/analytics/dashboard?since=-44640`,
+              CloudFlareFetchConfig
+            )
+              .then((response) => response.json())
+              .then((resp) => bandwidths.push(resp.result.totals.bandwidth.all))
+              .then(() => {
+                const sum = bandwidths.reduce((a, b) => a + b, 0);
+                const total = sum + VCband;
+                if (i++ === resp.result.length - 1) {
+                  res.setHeader("Cache-Control", "s-maxage=86400");
+                  res.status(200).send({
+                    status: "success",
+                    result: { bytes: total, humanReadable: formatBytes(total, 2) },
+                  });
+                }
               });
-            }
           });
-      });
-    })
-    .catch((err) => console.error(err));
+        })
+        .catch((err) => console.error(err));
+    });
 };
