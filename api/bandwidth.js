69a8d2ecebb31c9b6cfec1b3c406076acd16faf0
Restyled.io <commits@restyled.io>
2020-09-21 19:47:48 +0000

    Restyled by prettier

--- api/bandwidth.js
+++ api/bandwidth.js
@@ -10,26 +10,25 @@ const vercelEndpoint = "https://vercel.com/api/pricing/state/usage";
 
 const vercelToken = `Bearer ${VCAuth}`;
 const VercelFetchConfig = {
-  method : "GET",
-  headers : {
-    Authorization : vercelToken,
+  method: "GET",
+  headers: {
+    Authorization: vercelToken,
   },
 };
 
 const cloudflareToken = `Bearer ${CFAuth}`;
 const CloudFlareFetchConfig = {
-  method : "GET",
-  headers : {
-    Authorization : cloudflareToken,
+  method: "GET",
+  headers: {
+    Authorization: cloudflareToken,
   },
 };
 
 function formatBytes(bytes, decimals = 0) {
-  if (bytes === 0)
-    return "0 Bytes";
+  if (bytes === 0) return "0 Bytes";
   const k = 1024;
   const dm = decimals < 0 ? 0 : decimals;
-  const sizes = [ "Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB" ];
+  const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
   const i = Math.floor(Math.log(bytes) / Math.log(k));
   return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i];
 }
@@ -38,39 +37,43 @@ export default (_req, res) => {
   let CloudFlareBandwidths = [];
 
   fetch(vercelEndpoint, VercelFetchConfig)
-      .then((responce) => responce.json())
-      .then((Vresp) => {
-        const VCband = Vresp.metrics.bandwidth.rx + Vresp.metrics.bandwidth.tx;
+    .then((responce) => responce.json())
+    .then((Vresp) => {
+      const VCband = Vresp.metrics.bandwidth.rx + Vresp.metrics.bandwidth.tx;
 
-        /* Get all the zones */
+      /* Get all the zones */
 
-        fetch(listEndpoint, CloudFlareFetchConfig)
-            .then((response) => response.json())
-            .then((resp) => {
-              let i = 0;
-              resp.result.forEach((obj) => {
-                fetch(`https://api.cloudflare.com/client/v4/zones/${
-                          obj.id}/analytics/dashboard?since=-44640`,
-                      CloudFlareFetchConfig)
-                    .then((response) => response.json())
-                    .then((resp) => CloudFlareBandwidths.push(
-                              resp.result.totals.bandwidth.all))
-                    .then(() => {
-                      const CloudFlareSum =
-                          CloudFlareBandwidths.reduce((a, b) => a + b, 0);
-                      const total = CloudFlareSum + VCband;
-                      if (i++ === resp.result.length - 1) {
-                        res.setHeader("Cache-Control", "s-maxage=86400");
-                        res.status(200).send({
-                          status : "success",
-                          result : {
-                            bytes : total,
-                            humanReadable : formatBytes(total, 2),
-                          },
-                        });
-                      }
-                    });
+      fetch(listEndpoint, CloudFlareFetchConfig)
+        .then((response) => response.json())
+        .then((resp) => {
+          let i = 0;
+          resp.result.forEach((obj) => {
+            fetch(
+              `https://api.cloudflare.com/client/v4/zones/${obj.id}/analytics/dashboard?since=-44640`,
+              CloudFlareFetchConfig
+            )
+              .then((response) => response.json())
+              .then((resp) =>
+                CloudFlareBandwidths.push(resp.result.totals.bandwidth.all)
+              )
+              .then(() => {
+                const CloudFlareSum = CloudFlareBandwidths.reduce(
+                  (a, b) => a + b,
+                  0
+                );
+                const total = CloudFlareSum + VCband;
+                if (i++ === resp.result.length - 1) {
+                  res.setHeader("Cache-Control", "s-maxage=86400");
+                  res.status(200).send({
+                    status: "success",
+                    result: {
+                      bytes: total,
+                      humanReadable: formatBytes(total, 2),
+                    },
+                  });
+                }
               });
-            });
-      });
+          });
+        });
+    });
 };
