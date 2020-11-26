e340663a7fc38369fdcb582fff2c9916e3009203
Filip Troníček <filip.tronicek@seznam.cz>
2020-11-26 12:44:33 +0100

    Add types and TS to bandwidth

--- api/bandwidth.ts
+++ api/bandwidth.ts
@@ -1,45 +1,46 @@
+import { NowResponse } from '@vercel/node';
 import fetch from "node-fetch";
 require("dotenv").config();
 
-const CFAuth = process.env.CF_TOKEN;
-const VCAuth = process.env.VC_TOKEN;
+const CFAuth: string | undefined = process.env.CF_TOKEN;
+const VCAuth: string | undefined = process.env.VC_TOKEN;
 
 /* Config */
-const listEndpoint = "https://api.cloudflare.com/client/v4/zones";
-const vercelEndpoint = "https://vercel.com/api/pricing/state/usage";
+const listEndpoint : string = "https://api.cloudflare.com/client/v4/zones";
+const vercelEndpoint : string = "https://vercel.com/api/pricing/state/usage";
 
-const vercelToken = `Bearer ${VCAuth}`;
-const VercelFetchConfig = {
+const vercelToken : string = `Bearer ${VCAuth}`;
+const VercelFetchConfig : object = {
   method: "GET",
   headers: {
     Authorization: vercelToken,
   },
 };
 
-const cloudflareToken = `Bearer ${CFAuth}`;
-const CloudFlareFetchConfig = {
+const cloudflareToken : string = `Bearer ${CFAuth}`;
+const CloudFlareFetchConfig : object = {
   method: "GET",
   headers: {
     Authorization: cloudflareToken,
   },
 };
 
-function formatBytes(bytes, decimals = 0) {
+function formatBytes(bytes : number, decimals : number = 0) : string {
   if (bytes === 0) return "0 Bytes";
-  const k = 1024;
-  const dm = decimals < 0 ? 0 : decimals;
-  const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
-  const i = Math.floor(Math.log(bytes) / Math.log(k));
+  const k : number = 1024;
+  const dm : number = decimals < 0 ? 0 : decimals;
+  const sizes : string[] = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+  const i : number = Math.floor(Math.log(bytes) / Math.log(k));
   return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i];
 }
 
-export default (_req, res) => {
-  let CloudFlareBandwidths = [];
+export default (_req, res: NowResponse) => {
+  const CloudFlareBandwidths : number[] = [];
 
   fetch(vercelEndpoint, VercelFetchConfig)
     .then((responce) => responce.json())
     .then((Vresp) => {
-      const VCband = Vresp.metrics.bandwidth.rx + Vresp.metrics.bandwidth.tx;
+      const VCband : number = Vresp.metrics.bandwidth.rx + Vresp.metrics.bandwidth.tx;
 
       /* Get all the zones */
 
@@ -57,11 +58,11 @@ export default (_req, res) => {
                 CloudFlareBandwidths.push(resp.result.totals.bandwidth.all)
               )
               .then(() => {
-                const CloudFlareSum = CloudFlareBandwidths.reduce(
+                const CloudFlareSum : number = CloudFlareBandwidths.reduce(
                   (a, b) => a + b,
                   0
                 );
-                const total = CloudFlareSum + VCband;
+                const total : number = CloudFlareSum + VCband;
                 if (i++ === resp.result.length - 1) {
                   res.setHeader("Cache-Control", "s-maxage=86400");
                   res.status(200).send({
