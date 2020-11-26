4aee4fc8d86077b83b23c9c6a24c979bf558e986
Filip Troníček <filip.tronicek@seznam.cz>
2020-11-26 12:28:54 +0100

    Convert to TS

--- api/status.ts
+++ api/status.ts
@@ -1,14 +1,15 @@
+import { NowResponse } from '@vercel/node';
 import request from "request";
 
 const successCodes = [200, 201, 202, 204, 301, 302, 307, 308];
 
-export default (req, res) => {
+export default (req: any, res: NowResponse) => {
 
   const urlList = req.query.url && req.query.url.split(",") || ["https://trnck.dev", "https://blog.trnck.dev"];
 
-  function getStatus(url) {
+  function getStatus(url: string): any {
     return new Promise((resolve, _reject) => {
-      request(url, (error, response, _body) => {
+      request(url, (error: any, response: any, _body: string) => {
               resolve({
                   site: url,
                   status: !error && successCodes.includes(response.statusCode)
@@ -19,7 +20,7 @@ export default (req, res) => {
     });
   }
 
-  const promiseList = urlList.map((url) => getStatus(url));
+  const promiseList = urlList.map((url: string) => getStatus(url));
 
   Promise.all(promiseList).then((resultList) => {
     const jsonString = ({status: "success", result: resultList });
