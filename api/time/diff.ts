b1418cd3f644e9e3393811961b5c4a33185284d5
Filip Troníček <filip.tronicek@seznam.cz>
2020-11-27 12:26:46 +0100

    Update diff.ts

--- api/time/diff.ts
+++ api/time/diff.ts
@@ -4,6 +4,5 @@ export default (request: NowRequest, response: NowResponse) => {
   const now = new Date().getTime();
   const clientnow = request.query.ts;
   const clientTimeStamp = parseInt(clientnow);
-  console.log(clientTimeStamp);
   response.status(200).send({"status": "success", "result": { s: Math.round((now - clientTimeStamp) / 1000), ms: now - clientTimeStamp}});
 }
