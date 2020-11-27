1c88d014ebf013ba305350f96432f111398e47eb
Filip Troníček <filip.tronicek@seznam.cz>
2020-11-27 12:20:42 +0100

    Easy peasy difference function

---
+++ api/time/diff.ts
@@ -0,0 +1,9 @@
+import { NowRequest, NowResponse } from '@vercel/node';
+
+export default (request: NowRequest, response: NowResponse) => {
+  const now = new Date().getTime();
+  const clientnow = request.query.ts;
+  const clientTimeStamp = parseInt(clientnow);
+  console.log(clientTimeStamp);
+  response.status(200).send({"status": "success", "result": { s: Math.round((now - clientTimeStamp) / 1000), ms: now - clientTimeStamp}});
+}
