3aa0d5625ed742cd4aecdbf74bd1856f0ebd1656
Filip Troníček <filip.tronicek@seznam.cz>
2021-01-30 18:27:15 +0100

    Add current timestamp

--- api/time/diff.ts
+++ api/time/diff.ts
@@ -4,5 +4,5 @@ export default (request: NowRequest, response: NowResponse) => {
   const now = new Date().getTime();
   const clientnow = request.query.ts;
   const clientTimeStamp = parseInt(clientnow);
-  response.status(200).send({"status": "success", "result": { s: Math.round((now - clientTimeStamp) / 1000), ms: now - clientTimeStamp}});
+  response.status(200).send({"status": "success", "result": { s: Math.round((now - clientTimeStamp) / 1000), ms: now - clientTimeStamp, unix: now }});
 }
